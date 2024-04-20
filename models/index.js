b99d0747e1e677452e28122fb0bb20cd9c0f1db5
Francisco Wallison <franciscowallison@gmail.com>
2024-04-20 11:16:43 -0300

    add variaveis para o bancdo de dados

--- models/index.js
+++ models/index.js
@@ -1,45 +1,57 @@
-'use strict';
-require('dotenv').config()
-const mariadb = require('mariadb');
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+"use strict";
+require("dotenv").config();
+const mariadb = require("mariadb");
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 
+const config = {
+  username: process.env.api_username,
+  password: process.env.api_password,
+  database: process.env.api_database,
+  host: process.env.api_host,
+  port: process.env.api_port,
+  dialect: "mariadb",
+};
 
-if (config.dialect === 'mariadb') {
+if (config.dialect === "mariadb") {
   config.dialectModule = mariadb;
 }
 
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
