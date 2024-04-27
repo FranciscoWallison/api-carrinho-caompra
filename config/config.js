c012fb8dcf420f30beb87644d105b52bb389f744
Francisco Wallison <franciscowallison@gmail.com>
2024-04-27 09:16:58 -0300

    add new config supabase

--- config/config.js
+++ config/config.js
@@ -1,13 +1,30 @@
 require("dotenv").config();
 
+console.log(
+  process.env.API_USERNAME,
+  process.env.API_PASSWORD,
+  process.env.API_DATABASE,
+  process.env.API_HOST,
+  process.env.API_PORT,
+  process.env.API_DIALECT,
+  process.env.API_LOGGING
+);
+
 module.exports = {
   development: {
-    username: "root",
-    password: "123456",
-    database: "HML_CarrinhoCompra",
-    host: "127.0.0.1",
-    dialect: "mariadb",
-    port: "3333"
+    username: process.env.API_USERNAME,
+    password: process.env.API_PASSWORD,
+    database: process.env.API_DATABASE,
+    host: process.env.API_HOST,
+    port: process.env.API_PORT,
+    dialect: process.env.API_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: process.env.API_LOGGING === "true" ? console.log : false,
   },
   test: {
     username: "root",
@@ -15,13 +32,21 @@ module.exports = {
     database: "TesteCarrinhoCompra",
     host: "127.0.0.1",
     dialect: "mariadb",
-    port: "3333"
+    port: "3333",
   },
   production: {
-    username: "root",
-    password: "123456",
-    database: "HML_CarrinhoCompra",
-    host: "127.0.0.1",
-    dialect: "mariadb",
+    username: process.env.API_USERNAME,
+    password: process.env.API_PASSWORD,
+    database: process.env.API_DATABASE,
+    host: process.env.API_HOST,
+    port: process.env.API_PORT,
+    dialect: process.env.API_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: process.env.API_LOGGING === "true" ? console.log : false,
   },
 };
--- models/index.js
+++ models/index.js
@@ -1,23 +1,23 @@
 "use strict";
 require("dotenv").config();
 const mariadb = require("mariadb");
+const postgres = require("pg");
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 
 const basename = path.basename(__filename);
+
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 
-const config = {
-  username: process.env.api_username,
-  password: process.env.api_password,
-  database: process.env.api_database,
-  host: process.env.api_host,
-  port: process.env.api_port,
-  dialect: "mariadb",
-};
+if (config.dialect === "postgres") {
+  config.dialectModule = postgres;
+}
 
 if (config.dialect === "mariadb") {
   config.dialectModule = mariadb;
